ae0f62ed3503a56cb8f9854f22804f7bcfde5394
Abdulrahman <albakkar997@gmail.com>
2023-03-22 06:04:44 +0300

    Assignment submitted

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {
+    name: "Abdulrahman Albakkar",
+    streetAddress: 1997
+}
+
+function updateEmployeeWithKeyAndValue(object, key, value){
+    const newObject = {...object}
+    newObject.key = value
+    return newObject
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value){
+    object[key] = value
+    return object
+}
+function deleteFromEmployeeByKey(object, key){
+    const newObject = {...object}
+    delete newObject[key]
+    return newObject
+}
+function destructivelyDeleteFromEmployeeByKey(object, key){
+    delete object[key]
+    return object
+}
